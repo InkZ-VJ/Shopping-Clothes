303009b332f60c910e607a198f37e16d7d780220
Inchree <Vatcharapong.j@hotmail.com>
2023-12-26 15:18:50 +0700

    created check page with functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import "./cart-dropdown.styles.scss";
 import { CartContext } from "../../contexts/cart.context";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
+    const navigate =useNavigate();
+
+    const goToCheckOutHandler = () =>{
+        navigate('/checkout')
+    }
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -13,7 +21,7 @@ const CartDropdown = () => {
             <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GOTO CHECKOUT</Button>
+      <Button onClick={goToCheckOutHandler}>GOTO CHECKOUT</Button>
     </div>
   );
 };
